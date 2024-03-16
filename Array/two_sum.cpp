3d9be5ea3da42451eca8d8d22d8d32826899c481
Pradhuman Thakurela <pradhumanthakurela@gmail.com>
2024-03-16 22:34:52 +0530

    16_march

---
+++ Array/two_sum.cpp
@@ -0,0 +1,22 @@
+/* The link of the problem is given below :-
+https://leetcode.com/problems/two-sum/
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int first = 0; first < nums.size(); first++){
+			//For every first ...
+            for (int second = first + 1; second < nums.size(); second++){
+				//... and second, ...
+                if (nums[first] + nums[second] == target){
+				//... if nums[first] + nums[second] == target then return {first, second}.
+                    return {first, second};
+                }
+            }
+        }
+		//If there is no solution return an empty vector.
+        return {};
+    }
+        
+    };
