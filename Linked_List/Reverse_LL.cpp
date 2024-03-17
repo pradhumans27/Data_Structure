35427451c93909c955321a6929c47a67a6b0b5e2
Pradhuman Thakurela <pradhumanthakurela@gmail.com>
2024-03-18 00:25:34 +0530

    18_march

---
+++ Linked_List/Reverse_LL.cpp
@@ -0,0 +1,25 @@
+/* The link of the problem is given below:-
+https://leetcode.com/problems/reverse-linked-list/ 
+*/
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* curr = head;
+        ListNode* forward = NULL;
+        ListNode* previous = NULL;
+        if(head==NULL || head->next ==NULL)
+        {
+            return head;
+        }
+        while(curr!=NULL)
+        {
+            forward = curr->next;
+            curr->next = previous;
+            previous = curr;
+            curr = forward;
+        }
+        head = previous;
+        return head;
+    }
+};
