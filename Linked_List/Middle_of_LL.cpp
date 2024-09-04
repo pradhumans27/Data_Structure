187732b5e9208654e54380936f28098aed74f3d6
unknown <pradhumanthakurela@gmail.com>
2024-09-04 13:02:10 +0530

    Optmized

--- Linked_List/Middle_of_LL.cpp
+++ Linked_List/Middle_of_LL.cpp
@@ -4,25 +4,21 @@ https://leetcode.com/problems/middle-of-the-linked-list/
 
 class Solution {
 public:
-    int length(ListNode* head) {
-        ListNode* temp = head;
-        int len = 0;
-        while (temp != NULL) {
-            temp = temp->next;
-            len++;
-        }
-        return len;
-    }
     ListNode* middleNode(ListNode* head) {
-        int range = length(head);
-         int ans = range/2;
-        int cnt = 0;
-        ListNode* temp = head;
-        while (cnt < ans) {
-            temp = temp->next;
-            cnt++;
+        if(head==NULL || head->next==NULL)
+        return head;
+        ListNode*fast = head->next;
+        ListNode*slow = head;
+        while(fast!=NULL)
+        {
+            fast = fast->next;
+            if(fast!=NULL)
+            {
+                fast = fast->next;
+            }
+            slow = slow->next;
+            
         }
-
-        return temp;
+        return slow;
     }
 };
