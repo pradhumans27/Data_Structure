a6e5c441ebbaef13ff7dd22ef5149c4d52bd6f6d
unknown <pradhumanthakurela@gmail.com>
2024-10-02 00:20:54 +0530

    optimized

--- Array/container_with_most_water.cpp
+++ Array/container_with_most_water.cpp
@@ -5,20 +5,19 @@ class Solution {
 public:
     int maxArea(vector<int>& height) {
         int left = 0;
-        int right = height.size() - 1;
-        int maxArea = 0;
-
-        while (left < right) {
-            int currentArea = min(height[left], height[right]) * (right - left);
-            maxArea = max(maxArea, currentArea);
-
-            if (height[left] < height[right]) {
-                left++;
-            } else {
-                right--;
-            }
+        int right = height.size()-1;
+        int area = 0;
+        while(left<right)
+        {
+            int lh = height[left];
+            int rh = height[right];
+            int min_height = min(lh,rh);
+            area = max(area,min_height*(right-left));
+            if(lh>rh)
+            right--;
+            else
+            left++;
         }
-
-        return maxArea;
+        return area;
     }
 };
